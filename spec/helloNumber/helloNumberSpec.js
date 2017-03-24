1fce1be6d8e21afd5ecebf986159c3a2799b7711
TianGuang He <tigarhe@thoughtworks.com>
2017-03-24 15:53:56 +0800

    modify test code

--- spec/helloNumber/helloNumberSpec.js
+++ spec/helloNumber/helloNumberSpec.js
@@ -1,6 +1,6 @@
 describe("helloNumber", function() {
 
-    it("should return number itself" , function() {
+    it("when input number should return number String" , function() {
         var number = helloNumber(1);
 
         expect(number === "1").toBe(true)
