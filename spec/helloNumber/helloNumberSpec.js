d0f1522fb1dcd4132414c1f31871dd3a6fdff6fd
TianGuang He <tigarhe@thoughtworks.com>
2017-03-24 16:29:54 +0800

    add when input number of 3 multiple should return Fizz test

--- spec/helloNumber/helloNumberSpec.js
+++ spec/helloNumber/helloNumberSpec.js
@@ -1,10 +1,16 @@
 describe("helloNumber", function() {
 
     var helloNumber = require("../../lib/helloNumber/helloNumber");
-    
+
     it("when input number should return number String" , function() {
         var number = helloNumber(1);
 
         expect(number === "1").toBe(true)
+    })
+
+    it("when input number of 3 multiple should return Fizz", function() {
+        var number = helloNumber(3)
+
+        expect = helloNumber(number === "Fizz").toBe(true);
     })
 }) 
