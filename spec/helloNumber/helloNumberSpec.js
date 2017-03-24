879df8ec2e0be7742781070f626c9fd9c5953b49
TianGuang He <tigarhe@thoughtworks.com>
2017-03-24 15:45:55 +0800

    add a input number output number test

---
+++ spec/helloNumber/helloNumberSpec.js
@@ -0,0 +1,8 @@
+describe("helloNumber", function() {
+
+    it("should return number itself" , function() {
+        var number = helloNumber(1);
+
+        expect(number === "1").toBe(true)
+    })
+}) 
